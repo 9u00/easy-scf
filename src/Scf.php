c050d813444cea1a04bfdbaf6f63d442f9c0f7b7
9u0 <iamxzgsl@gmail.com>
2025-01-17 13:21:56 +0800

    修改自动加载路径

--- src/Scf.php
+++ src/Scf.php
@@ -4,6 +4,14 @@ namespace EasyScf;
 
 class Scf
 {
+    public $event;
+    public $context;
+    public function __construct($event, $context)
+    {
+        $this->event = $event;
+        $this->context = $context;
+    }
+
     public function run()
     {
         echo "Hello, World!";
