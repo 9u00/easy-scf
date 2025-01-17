f8dba872f0aa8d161edc7bb532c7ab21cf62f94e
9u0 <iamxzgsl@gmail.com>
2025-01-17 15:22:46 +0800

    增加通用模块,鉴权,配置

--- src/Scf.php
+++ src/Scf.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace EasyScf;
 
 class Scf
@@ -9,35 +8,65 @@ class Scf
     public $controller;
     public $model;
     public $validate;
-    public $routes;
+    public $headers;
     public $params;
     public $body;
     public function __construct($event, $context)
     {
         $this->event = $event;
         $this->context = $context;
-        $this->routes = require 'routes.php';
+        $this->headers = $event->headers;
     }
 
     public function run()
     {
         $route = new Route($this->event, $this->context->function_name);
-        list($isMatch, $controller, $action, $params, $body) = $route->init();
-        if (!$isMatch) {
-            var_dump('404');
-            return '404';
+        list ($item, $controller, $function, $params, $body) = $route->init();
+        if (!$item) {
+            return [
+                'code' => 404,
+                'msg' => '资源不存在'
+            ];
         }
-        var_dump([
-            'controller' => $controller,
-            'action' => $action,
-            'params' => $params,
-            'body' => $body,
-        ]);
-        return [
-            'controller' => $controller,
-            'action' => $action,
-            'params' => $params,
-            'body' => $body,
-        ];
+
+        //数据库链接
+        list($dbRead, $db, $redis) = (new \EasyScf\Db)->init(6);
+
+        $cName = '\Controller\\' . $controller;
+        $c = new $cName($db, $dbRead, $redis);
+
+        //禁用函数
+        if (in_array($function, $c->disabledFun) || $c->disabledFun == '*') {
+            return [
+                'code' => 404,
+                'msg' => '资源不存在或禁用'
+            ];
+        }
+
+        //鉴权
+        $auth = new Auth($db, $dbRead, $redis);
+        if (!$auth->authCheck($this->headers, $c->needLoginFun, $function)) {
+            return $auth->getResponse();
+        }
+
+        if ($auth->uid && $this->event->httpMethod != 'GET' && $redis) {
+            //处理同一用户并发写入
+            $key = 'mbd_user_' . $controller . '_' . $function . '_' . $auth->uid;
+            $item = $redis->setNx($key, 1);
+            if (!$item) {
+                return $c->error('请勿重复提交', 429);
+            }
+            $redis->expire($key, 30);
+        }
+
+        $c->setUid($auth->uid);
+        $c->setUser($auth->user);
+   
+        $result = $c->$function($params, $body);
+        if ($auth->uid && $this->event->httpMethod != 'GET' && $redis) {
+            $redis->del($key);
+        }
+
+        return $result;
     }
 }
---
+++ src/config.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'userModel' => 'user',
+    'db' => [
+        'read' => [
+            'database_type' => 'mysql',
+            'server' => '127.0.0.1',
+            'username' => 'root',
+            'password' => '123456',
+            'database_name' => 'test',
+            'port' => '3306',
+        ],
+        'write' => [
+            'database_type' => 'mysql',
+            'server' => '127.0.0.1',
+            'username' => 'root',
+            'password' => '123456',
+            'database_name' => 'test',
+            'port' => '3306',
+        ],
+        'redis' => [
+            'host' => '127.0.0.1',
+            'port' => 6379,
+            'password' => '123456',
+        ]
+    ]
+];
